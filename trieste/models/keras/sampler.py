f553199e50890c4b22651af4bf76094c55a9254b
ampudia19 <dampudiavicente@gmail.com>
2022-04-16 18:23:56 +0200

    debug sampler

--- trieste/models/keras/sampler.py
+++ trieste/models/keras/sampler.py
@@ -41,6 +41,7 @@ class EnsembleTrajectorySampler(TrajectorySampler[EnsembleModel]):
     def __init__(self, model: EnsembleModel, use_samples: bool = False):
         """
         :param model: The ensemble model to sample from.
+        :param use_samples:
         """
         if not isinstance(model, EnsembleModel):
             raise NotImplementedError(
@@ -68,6 +69,20 @@ class EnsembleTrajectorySampler(TrajectorySampler[EnsembleModel]):
         """
         return ensemble_trajectory(self._model, self._use_samples)
 
+    def update_trajectory(self, trajectory: TrajectoryFunction) -> TrajectoryFunction:
+        """
+        Update a :const:`TrajectoryFunction` to reflect an update in its
+        underlying :class:`ProbabilisticModel` and resample accordingly.
+
+        Efficient implementations will have a custom method here to allow in-place resampling
+        and updating. However, the default behavior is just to make a new trajectory from scratch.
+
+        :param trajectory: The trajectory function to be resampled.
+        :return: The new trajectory function updated for a new model
+        """
+        tf.debugging.Assert(isinstance(trajectory, ensemble_trajectory), [])
+        return trajectory  # nothing to update
+
     def resample_trajectory(self, trajectory: TrajectoryFunction) -> TrajectoryFunction:
         """
         Efficiently resample a :const:`TrajectoryFunction` in-place to avoid function retracing
@@ -90,63 +105,74 @@ class ensemble_trajectory(TrajectoryFunctionClass):
     def __init__(self, model: EnsembleModel, use_samples: bool):
         """
         :param model: The model of the objective function.
+        :param use_samples:
         """
         self._model = model
         self._use_samples = use_samples
+
         self._initialized = tf.Variable(False, trainable=False)
+        self._batch_size = tf.Variable(0, dtype=tf.int32, trainable=False)
+        self._indices = tf.Variable(tf.zeros([0], dtype=tf.int32), shape=[None], trainable=False)
 
-        # dummy inits to be updated before trajectory evaluation
-        self._indices = tf.Variable(tf.ones([0,], dtype=tf.int32), shape=[None], trainable=False)
-        self._batch_size = tf.Variable(
-            0, dtype=tf.int32, trainable=False
-        )
         if self._use_samples:
-            self._seeds = tf.Variable(tf.ones([0,0], dtype=tf.int32), shape=[None, None], trainable=False)
+            self._ensemble_size = self._model.ensemble_size
+            self._sample_size = tf.Variable(0, dtype=tf.int32, trainable=False)
+            self._seeds = tf.Variable(
+                tf.zeros([self._ensemble_size, 2], dtype=tf.int32), trainable=False
+            )
 
-    # @tf.function
+    @tf.function
     def __call__(self, x: TensorType) -> TensorType:  # [N, B, d] -> [N, B]
         """
         Call trajectory function. Note that we are flattening the batch dimension and
         doing a forward pass with each network in the ensemble with the whole batch. This is
         somewhat wasteful, but is necessary given the underlying ``KerasEnsemble`` network
         model. Also, if same networks are used in multiple batch elements due to sampling
-        with replacement it is less wasteful. 
+        with replacement it is less wasteful.
         """
         if not self._initialized:  # work out desired batch size from input
             self._batch_size.assign(tf.shape(x)[-2])  # B
-            self.resample()  # sample B network indices
+            if self._use_samples:
+                self._sample_size.assign(-(-self._batch_size // self._ensemble_size))  # B/E
+            self.resample()  # sample network indices/seeds
             self._initialized.assign(True)
 
         tf.debugging.assert_equal(
             tf.shape(x)[-2],
-            self._batch_size.value(),
+            self._batch_size,
             message=f"""
             This trajectory only supports batch sizes of {self._batch_size}.
             If you wish to change the batch size you must get a new trajectory
             by calling the get_trajectory method of the trajectory sampler.
             """,
         )
-
         flat_x, unflatten = flatten_leading_dims(x)  # [N*B, d]
-        x_transformed = self._model.prepare_query_points(flat_x)
-        ensemble_distributions = self._model.model(x_transformed)
-        # [DAV] *** ValueError: Input 0 of layer model_2_dense_0 is incompatible with the layer: expected axis -1 of input shape to have value 1 but received input with shape (4, 2)
-        predictions = []
-        batch_index = tf.range(0, self._batch_size, 1)
-        if self._use_samples: 
-            for b, seed in zip(batch_index, tf.unstack(self._seeds)):
-                predictions.append(ensemble_distributions[self._indices[b]].sample(seed=seed))
+        ensemble_distributions = self._model.ensemble_distributions(flat_x)
+
+        if self._use_samples:
+            samples = tf.convert_to_tensor(
+                [
+                    dist.sample(sample_shape=self._sample_size, seed=self._seeds[i])
+                    for i, dist in enumerate(ensemble_distributions)
+                ]
+            )  # [E, B/E, N*B, 1]
+            flattened_samples = tf.reshape(
+                samples, [self._ensemble_size * self._sample_size, *samples.shape[2:]]
+            )  # [E*B/E, N*B, 1]
+            predictions = tf.gather(flattened_samples, self._indices)  # [B, N*B, 1]
         else:
-            for b in batch_index:
-                predictions.append(ensemble_distributions[self._indices[b]].mean())
-        tensor_predictions = tf.squeeze(
-            tf.convert_to_tensor([unflatten(p) for p in predictions]), axis=-1
-        )  # [B, N, B]
+            predicted_means = tf.convert_to_tensor([dist.mean() for dist in ensemble_distributions])
+            predictions = tf.gather(predicted_means, self._indices)  # [B, N*B, 1]
+
+        tensor_predictions = tf.squeeze(tf.map_fn(unflatten, predictions), axis=-1)  # [B, N, B]
 
         # here we select simultaneously networks and batch dimension according to batch indices
         # this is needed because we compute a whole batch with each network
+        batch_index = tf.range(self._batch_size)
         indices = tf.stack([batch_index, batch_index], axis=1)
-        batch_predictions = tf.gather_nd(tf.transpose(tensor_predictions, perm=[0,2,1]), indices)  # [B,N]
+        batch_predictions = tf.gather_nd(
+            tf.transpose(tensor_predictions, perm=[0, 2, 1]), indices
+        )  # [B,N]
 
         return tf.transpose(batch_predictions, perm=[1, 0])  # [N, B]
 
@@ -154,10 +180,16 @@ class ensemble_trajectory(TrajectoryFunctionClass):
         """
         Efficiently resample network indices, and optionally quantiles, in-place without retracing.
         """
-        self._indices.assign(self._model.sample_index(self._batch_size))  # [B]
         if self._use_samples:
-            self._seeds.assign(tf.random.uniform(shape=(self._batch_size, 2), minval=1, maxval=999999999, dtype=tf.int32))
-
+            indices = tf.random.shuffle(tf.range(self._ensemble_size * self._sample_size))
+            self._indices.assign(indices[: self._batch_size])  # [B]
+            self._seeds.assign(
+                tf.random.uniform(
+                    shape=(self._ensemble_size, 2), minval=1, maxval=999999999, dtype=tf.int32
+                )
+            )  # [E, 2]
+        else:
+            self._indices.assign(self._model.sample_index(self._batch_size))  # [B]
 
 class DropoutTrajectorySampler(TrajectorySampler[TrainableProbabilisticModel]):
     """
